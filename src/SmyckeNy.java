f7b25c359ee4bcd2488d7f5daeed9d21556cdcfe
lindberglove <lovelindberg2@gmail.com>
2018-06-05 15:57:28 +0200

    Add files via upload

--- src/SmyckeNy.java
+++ src/SmyckeNy.java
@@ -1,29 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
 
-class SmyckeNy extends JPanel {
-    private JTextField nfält = new JTextField(10);
-    private JTextField afält = new JTextField(10);
+class SmyckeNy extends JFrame {
+  JTextField nfält = new JTextField(10);
+  JTextField afält = new JTextField(10);
 
-    public SmyckeNy() {
-
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        JPanel rad1 = new JPanel();
-        rad1.add(new JLabel("Namn:"));
-        rad1.add(nfält);
-        add(rad1);
-        JPanel rad2 = new JPanel();
-        rad2.add(new JLabel("Antal:"));
-        rad2.add(afält);
-        add(rad2);
+    SmyckeNy() {
+		
+			setLayout(new FlowLayout());
+			JPanel rad1 = new JPanel();
+			rad1.add(new JLabel("Namn:"));
+			rad1.add(nfält);
+			add(rad1);
+			JPanel rad2 = new JPanel();
+			rad2.add(new JLabel("test:"));
+			rad2.add(nfält);
+			add(rad2);
+			setSize(230, 100); 
+			setVisible(true);
         
     }
 
-    public String getNamn() {
+   /* public String getNamn() {
         return nfält.getText();
     }
 
     public int getAntal() {
         return Integer.parseInt(afält.getText());
+       
+    }*/
+    
+    public static void main(String[] args){ new SmyckeNy(); 
     }
 }
+
